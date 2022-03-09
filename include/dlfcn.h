4eacce5354ca7810ec80cf49ac0f46f41a9343dd
Ivan Epifanov <isage.dna@gmail.com>
2022-03-09 11:35:48 +0300

    Add RTLD_ defines

--- include/dlfcn.h
+++ include/dlfcn.h
@@ -1,6 +1,11 @@
 #ifndef _DLFCN_H
 #define _DLFCN_H 1
 
+#define RTLD_LAZY 0x00001
+#define RTLD_NOW 0x00002
+#define RTLD_GLOBAL 0x00100
+#define RTLD_LOCAL 0x0
+
 extern void *dlopen (const char *__file, int __mode);
 
 extern int dlclose (void *__handle);
